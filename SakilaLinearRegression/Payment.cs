1eed51231ebc153655a6987cca6da8f0463ce60d
twofourZ <uppenberg95@gmail.com>
2024-11-11 16:15:26 +0100

    Implemented display for cost/rental-rental/cost by customer from Sakila

---
+++ SakilaLinearRegression/Payment.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SakilaLinearRegression
+{
+    internal class Payment
+    {
+        [Key]
+        public int payment_id { get; set; }
+        public int customer_id { get; set; }
+        public byte staff_id { get; set; }
+        public int? rental_id { get; set; }
+        public decimal amount { get; set; }
+        public DateTime payment_date { get; set; }
+        public DateTime last_update { get; set; }
+    }
+}
---
+++ SakilaLinearRegression/SakilaDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SakilaLinearRegression
+{
+    class SakilaDbContext : DbContext
+    {
+        private readonly string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Sakila;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+        public DbSet<Payment> payment { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
